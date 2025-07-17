d9d720f112d0a5485bb58b2525a46d58e6ad1889
Gnana Prakash M <gnanaprakash2918@gmail.com>
2025-07-17 13:48:35 +0530

    Add Item Data model Class

---
+++ src/main/java/model/Item.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    private String itemID;
+    private String itemName;
+    private double price;
+}
