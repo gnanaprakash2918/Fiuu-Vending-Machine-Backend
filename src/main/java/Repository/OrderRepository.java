3a65ea40686d84b42ba3e7fdbbd81488523ce7b2
Gnana Prakash M <gnanaprakash2918@gmail.com>
2025-07-17 14:17:29 +0530

    Added Repository

---
+++ src/main/java/Repository/OrderRepository.java
@@ -0,0 +1,10 @@
+// OrderRepository.java
+package Repository;
+
+import model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, String> {
+}
